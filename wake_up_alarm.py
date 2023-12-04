96f0ce61bec51955f97bda227a7560231d7debaf
Srilekha-03 <147143658+Srilekha-03@users.noreply.github.com>
2023-12-04 21:27:59 +0530

    Create wake_up_alarm.py
    Features:
    User-friendly Alarm Setup:
    Set your wake-up time by entering the date and time in the format dd/mm/yy hh/mm/ss when prompted by the script.
    
    Desktop Notifications:
    Receive desktop notifications using the win10toast library, ensuring that you're alerted even if the script is running in the background.
    
    Customizable Alarm Sound:
    Easily customize the alarm sound by modifying the file path within the script. The default sound (sound.mp3) can be replaced with any preferred audio file.
    
    Usage:
    Run the Script:
    Execute the script by running wake_up_alarm.py.
    
    Set the Alarm:
    Enter the desired wake-up date and time when prompted.
    
    Wake Up Prompt:
    The script will wait until the specified time and then trigger a desktop notification and play the chosen sound, ensuring you wake up on time.
    
    Customization and Contribution:
    Customize Sound:
    Replace the default sound.mp3 file with your preferred alarm sound.
    
    Contribute:
    Feel free to contribute improvements or additional features to enhance the functionality of this simple alarm clock script.
    
    Notes:
    Ensure that the pygame library is installed (pip install pygame) for proper sound functionality.
    
    The script is designed to be straightforward and serves as a foundation for more advanced alarm features.

---
+++ wake_up_alarm.py
@@ -0,0 +1,24 @@
+import pygame
+import win10toast
+from win10toast import ToastNotifier
+import time
+import datetime
+
+pygame.mixer.init()
+
+toaster = ToastNotifier()
+
+datetimeString = input("enter date and time in thsi format dd/mm/yy hh/mm/ss")
+today = datetime.datetime.today()
+target = datetime.datetime.strptime(datetimeString, "%d/%m/%Y %H:%M:%S")
+totalTime = target - today
+seconds = totalTime.total_seconds()
+
+
+print("\nALARM CLOCK SET SUCCESSFULLY\n")
+time.sleep(seconds)
+toaster.show_toast("Alarm clock","Wake Up",duration=10, threaded=True)
+pygame.mixer.music.load(r"C:\Users\srile\Downloads\sound.mp3")
+#pygame.mixer.music.load("path/to/sound.mp3")
+
+pygame.mixer.music.play()
